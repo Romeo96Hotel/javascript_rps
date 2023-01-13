3450bc0c221894ac35f077c94ae6f1c11008db99
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-13 15:47:20 -0300

    Add files via upload
    initial upload

---
+++ rps.js
@@ -0,0 +1,23 @@
+/* VARS */
+const userInput = prompt("Welcome to Rock Paper Scissors");
+const computerSelection = computerRandomSelection();
+
+/* functions */
+
+function computerRandomSelection() {
+  const choices = ["rock", "paper", "scissors"];
+  let random;
+  return random;
+}
+
+function calculateWinner() {
+  let winner;
+  let loser;
+  let computerSelection;
+  let playerSelection;
+}
+
+function displayWinner() {
+  console.log(winner);
+}
+/* end functions */
