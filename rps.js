ba2e525bb801ea78eb7fcc6a6852455e71102eb3
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-15 13:36:04 -0300

    Added for loop
    rps.js (added for loop that runs the game 3 times)

--- rps.js
+++ rps.js
@@ -1,34 +1,39 @@
-/* VARS */
-const userInput = prompt("Welcome to Rock Paper Scissors");
-const computerSelection = computerRandomSelection();
-let computerScore = 0;
-let playerScore = 0;
-let totalScore = "Player score: " + playerScore + "Computer score: " + computerScore;
+for (let i = 0; i < 4; i++) {
+  /* loops 4 times */
 
-/* functions */
+  /* VARS */
+  const userInput = prompt("Welcome to Rock Paper Scissors");
+  const computerSelection = computerRandomSelection();
+  let computerScore = 0;
+  let playerScore = 0;
+  let totalScore =
+    "Player score: " + playerScore + "Computer score: " + computerScore;
 
-function computerRandomSelection() {
-  const choices = ["rock", "paper", "scissors"];
-  let random;
-  return random;
-}
+  /* functions */
 
-function calculateWinner(computerSelection, userInput) {
-  this.computerSelection = toString(computerSelection);
-  this.userInput = toString(userInput);
-
-  if (computerSelection === "rock" && userInput === "scissors") {
-    computerScore++;
-    console.log("You lose round" + totalScore);
-  } else if (computerSelection === "paper" && userInput === "rock") {
-    computerScore++;
-    console.log("You lose round" + totalScore);
-  } else if (computerSelection === "scissors" && userInput === "paper") {
-    computerScore++;
-    console.log("You lose round" + totalScore);
-  } else {
-    console.log("Not a valid input");
+  function computerRandomSelection() {
+    const choices = ["rock", "paper", "scissors"];
+    let random;
+    return random;
   }
-}
 
-/* end functions */
+  function calculateWinner(computerSelection, userInput) {
+    this.computerSelection = toString(computerSelection);
+    this.userInput = toString(userInput);
+
+    if (computerSelection === "rock" && userInput === "scissors") {
+      computerScore++;
+      console.log("You lose round" + totalScore);
+    } else if (computerSelection === "paper" && userInput === "rock") {
+      computerScore++;
+      console.log("You lose round" + totalScore);
+    } else if (computerSelection === "scissors" && userInput === "paper") {
+      computerScore++;
+      console.log("You lose round" + totalScore);
+    } else {
+      console.log("Not a valid input");
+    }
+  }
+  /* end functions */
+}
+/* end for loop */
