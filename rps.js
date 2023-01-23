27858d8532f5be893199713df0bf0db9b0fbf9be
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-23 18:17:37 -0300

    Deleted some stuff

--- rps.js
+++ rps.js
@@ -1,38 +1,27 @@
 /* returns value of random index in array */
-const computerInput = function() {
+const computerInput = function () {
   const choices = ["rock", "paper", "scissors"];
   const random = choices[Math.floor(Math.random() * choices.length)];
   return random;
-}
+};
 
 /* returns user input */
-const userInput = function() {}
+const userInput = function () {
+  const buttons = document.querySelectorAll("button");
+};
 
 /* increments the counter */
-const updateCount = function(currentValue) {
+const updateCount = function (currentValue) {
   let currentCount = currentValue;
   currentCount++;
   return currentCount;
-}
+};
 
 /* sets new score after each game */
-const updateScore = function(score) {
+const updateScore = function (score) {
   newScore = score++;
   return newScore;
-}
-
-/* appends input taken from user to the display div */
-function appendUserInput(text) {
-  const newText = document.createElement("h1");
-  newText.appendChild(text);
-  return newText;
-}
-
-function appendComputerInput(text) {
-  const newText = document.createElement("h1");
-  newText.appendChild(text);
-  return newText;
-}
+};
 
 /* end functions */
 
@@ -42,3 +31,7 @@ let userCount = 0;
 let newUserCount = 0;
 let newComputerCount = 0;
 let computerCount = 0;
+
+const display = document.querySelector("#display");
+
+newText;
