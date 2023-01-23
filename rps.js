b24e63cbaf3cf59bd98c68f30a68a23e95cb7014
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-23 10:56:59 -0300

    Added stylesheet
    added css file to style buttons and display

--- rps.js
+++ rps.js
@@ -64,48 +64,48 @@ let userCount = 0;
 let newUserCount = 0;
 let newComputerCount = 0;
 let computerCount = 0;
+const computerChoice = computerRandomSelection();
+const userInput = prompt("rock, paper, or scissors?... ");
+const verifiedInput = verifyUserInput(userInput);
+const winner = roundWinner(userInput, computerChoice);
+const display = document.querySelector("#display");
 
-/* runs game 3 times */
-for (let i = 0; i < 5; i++) {
-  const computerChoice = computerRandomSelection();
-  const userInput = prompt("rock, paper, or scissors?... ");
-  const verifiedInput = verifyUserInput(userInput);
-  const winner = roundWinner(userInput, computerChoice);
 
-  /* if input is valid */
-  if (verifiedInput) {
-    console.log("user chooses " + userInput);
-    console.log("computer chooses " + computerChoice);
-    /* if player wins */
-    if (winner === 1) {
-      newUserCount = addToCount(userCount);
-      console.log("Player wins!");
-      /* if its a draw */
-    } else if (winner === 2) {
-      newUserCount = addToCount(userCount);
-      newComputerCount = addToCount(computerCount);
-      console.log("draw!");
-      /* computer wins */
-    } else {
-      newComputerCount = addToCount(computerCount);
-      console.log("computer wins!");
-    }
-    /* if input is not valid.. */
+/* if input is valid */
+if (verifiedInput) {
+  display.append(userInput);
+
+  console.log("user chooses " + userInput);
+  console.log("computer chooses " + computerChoice);
+  /* if player wins */
+  if (winner === 1) {
+    newUserCount = addToCount(userCount);
+    console.log("Player wins!");
+    /* if its a draw */
+  } else if (winner === 2) {
+    newUserCount = addToCount(userCount);
+    newComputerCount = addToCount(computerCount);
+    console.log("draw!");
+    /* computer wins */
   } else {
-    console.log("user chooses " + userInput + " Invalid input");
+    newComputerCount = addToCount(computerCount);
+    console.log("computer wins!");
   }
-
-  console.log(
-    "Player score: " +
-      newUserCount +
-      " / " +
-      "Computer score: " +
-      newComputerCount
-  );
-  /* set starting counts to the new count's value for new round*/
-  userCount = newUserCount;
-  computerCount = newComputerCount;
+  /* if input is not valid.. */
+} else {
+  console.log("user chooses " + userInput + " Invalid input");
 }
+
+console.log(
+  "Player score: " +
+    newUserCount +
+    " / " +
+    "Computer score: " +
+    newComputerCount
+);
+/* set starting counts to the new count's value for new round*/
+userCount = newUserCount;
+computerCount = newComputerCount;
 
 /* compares computer score to user score at the end of the game */
 if (computerCount > userCount) {
