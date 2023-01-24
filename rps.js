5ad211e0aa4fe4afef33386928793aa711839ea6
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-23 22:27:27 -0300

    Added functionality to playround function

--- rps.js
+++ rps.js
@@ -11,10 +11,24 @@ function updateScore(score) {
   return newScore;
 }
 
-function playRound(userInput, computerInput) {
-  userSelection = userInput;
-  computerSelection = computerInput;
-  
+/* plays a round of rock paper scissors and returns winner */
+function roundWinner(verifiedInput, computerChoice) {
+  const userInput = verifiedInput;
+  const computerInput = computerChoice;
+  let winner;
+
+  if (userInput === "rock" && computerInput === "scissors") {
+    winner = 1;
+  } else if (userInput === "paper" && computerInput === "rock") {
+    winner = 1;
+  } else if (userInput === "scissors" && computerInput === "paper") {
+    winner = 1;
+  } else if (userInput === computerInput) {
+    winner = 2;
+  } else {
+    winner = 3;
+  }
+  return winner;
 }
 
 /* end functions */
